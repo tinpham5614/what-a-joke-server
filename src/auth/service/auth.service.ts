a5aea554db68f525e83862e735c5c2f764a89381
Tin Pham <nhattin55@gmail.com>
2024-02-26 21:55:30 -0800

    update file structure

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { LoginDto } from './dto/login.dto';
+import { LoginDto } from '../dto/login.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/users/schema/user.schema';
 import { Model } from 'mongoose';
