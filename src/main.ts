39cb6cdec5a6623b3d6bf5ad7b5fcf7b95ef4ff4
Tin Pham <nhattin55@gmail.com>
2024-03-04 03:01:01 -0800

    update port

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
