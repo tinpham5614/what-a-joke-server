dc41952cb6395e21ff3d4cd3a898dc68620ee561
Tin Pham <nhattin55@gmail.com>
2024-03-05 01:14:41 -0800

    created jokes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JokesController } from './jokes/controller/jokes.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), UsersModule, AuthModule
   ],
-  controllers: [],
+  controllers: [JokesController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/jokes/dto/create-joke.dto.tsx
@@ -0,0 +1,20 @@
+import { IsDate, IsEmpty, IsNotEmpty, IsNumber, IsString } from "class-validator"
+import { User } from "src/users/schema/user.schema";
+
+export class CreateJokeDto {
+    @IsEmpty()
+    createdBy: User;
+
+    @IsString()
+    @IsNotEmpty()
+    joke: string;
+
+    @IsNumber()
+    favoriteCount: number;
+
+    @IsDate()
+    createdAt: Date;
+
+    @IsDate()
+    updatedAt: Date;
+}
