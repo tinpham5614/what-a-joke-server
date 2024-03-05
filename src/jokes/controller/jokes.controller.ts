8b2d519c83388888a94e4803e359b4b1b92aea4f
Tin Pham <nhattin55@gmail.com>
2024-03-05 01:46:38 -0800

    add co and ser for joke

--- src/jokes/controller/jokes.controller.ts
+++ src/jokes/controller/jokes.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { JokesService } from '../service/jokes/jokes.service';
+import { Joke } from '../schema/joke.schema';
+import { User } from 'src/users/schema/user.schema';
 
 @Controller('jokes')
-export class JokesController {}
+export class JokesController {
+  constructor(private readonly jokesService: JokesService) {}
+
+  // get all jokes sorted by createdAt
+  @Get('')
+  async getAllJokes(): Promise<Joke[]> {
+    return await this.jokesService.getAllJokes();
+  }
+
+  // get a joke by id
+  @Get(':id')
+  async getJokeById(id: string): Promise<Joke> {
+    return await this.jokesService.getJokeById(id);
+  }
+
+  // create a new joke
+  @Post('create')
+  async createJoke(@Body() joke: Joke, user: User): Promise<Joke> {
+    return await this.jokesService.createJoke(joke, user);
+  }
+
+  // update a joke by id
+  @Put('update/:id')
+  async updateJokeById(@Body() joke: Joke, id: string): Promise<Joke> {
+    return await this.jokesService.updateJokeById(id, joke);
+  }
+
+  // delete a joke by id
+  @Delete('delete/:id')
+  async deleteJokeById(id: string): Promise<Joke> {
+    return await this.jokesService.deleteJokeById(id);
+  }
+}
--- src/jokes/service/jokes/jokes.service.ts
+++ src/jokes/service/jokes/jokes.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Joke } from 'src/jokes/schema/joke.schema';
+import { Model } from 'mongoose';
+import mongoose from 'mongoose';
+import { User } from 'src/users/schema/user.schema';
 
 @Injectable()
-export class JokesService {}
+export class JokesService {
+  constructor(@InjectModel(Joke.name) private jokeModel: Model<Joke>) {}
+
+  // get all jokes sorted by createdAt
+  async getAllJokes(): Promise<Joke[]> {
+    const limitPerPage = 10;
+    const currentPage = 1;
+    const skip = (currentPage - 1) * limitPerPage;
+    return await this.jokeModel
+      .find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limitPerPage);
+  }
+
+  // get a joke by id
+  async getJokeById(id: string): Promise<Joke> {
+    const isValidJokeId = mongoose.isValidObjectId(id);
+    if (!isValidJokeId) {
+      throw new BadRequestException('Invalid joke id');
+    }
+    return await this.jokeModel.findById(id);
+  }
+  // create a new joke
+  async createJoke(joke: Joke, user: User): Promise<Joke> {
+    try {
+      joke.createdBy = user;
+      return await this.jokeModel.create(joke);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  // update a joke by id
+  async updateJokeById(id: string, joke: Joke): Promise<Joke> {
+    const isValidJokeId = mongoose.isValidObjectId(id);
+    if (!isValidJokeId) {
+      throw new BadRequestException('Invalid joke id');
+    }
+    return await this.jokeModel.findByIdAndUpdate(id, joke, { new: true });
+  }
+
+  // delete a joke by id
+  async deleteJokeById(id: string): Promise<Joke> {
+    const isValidJokeId = mongoose.isValidObjectId(id);
+    if (!isValidJokeId) {
+      throw new BadRequestException('Invalid joke id');
+    }
+    // find joke by id and update isDeleted to true
+    return await this.jokeModel.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true },
+    );
+  }
+}
