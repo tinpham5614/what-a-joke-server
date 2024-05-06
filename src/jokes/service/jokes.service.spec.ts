7749bdbe7a4464f737c34ff714a8421feab353d8
Tin Pham <nhattin55@gmail.com>
2024-05-06 01:12:56 -0700

    update tests

--- src/jokes/service/jokes.service.spec.ts
+++ src/jokes/service/jokes.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { JokesService } from './jokes.service';
+import { JokesService } from '../service/jokes.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Joke } from '../schema/joke.schema';
 
 describe('JokesService', () => {
   let service: JokesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JokesService],
+      providers: [
+        JokesService,
+        {
+          provide: getModelToken(Joke.name),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<JokesService>(JokesService);
   });
 
-  it('should be defined', () => {
+  it('service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllJokes', () => {
+    it('should return list of jokes', async () => {
+      const jokes = [
+        {
+          _id: '1234567890ab',
+          createdByUser: '0987654321ab',
+          joke: 'mocked_joke',
+          favoriteCount: 0,
+          isDeleted: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          _id: '0987654321ab',
+          createdByUser: '123456qweqwe',
+          joke: 'another_mocked_joke',
+          favoriteCount: 0,
+          isDeleted: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+      jest.spyOn(service, 'getAllJokes').mockResolvedValue(jokes as any);
+
+      const result = await service.getAllJokes();
+      expect(result).toEqual(jokes);
+    });
   });
 });
--- src/users/service/users.service.spec.ts
+++ src/users/service/users.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
-import { User } from '../schema/user.schema';
+import { UsersService } from '../service/users.service';
+import { Role, User } from '../schema/user.schema';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -8,13 +8,12 @@ describe('UsersService', () => {
   let service: UsersService;
   let userModel: Model<User>;
   const mockUser = {
-    _id: '1234567890ab',
+    id: '1234567890ab',
     firstName: 'mockFirstName',
     lastName: 'mockLastName',
     email: 'mockEmail',
-    password: 'mockPassword',
-    role: 'USER'
-  }
+    role: Role.USER,
+  } as unknown as User;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -40,30 +39,29 @@ describe('UsersService', () => {
         exec: jest.fn().mockResolvedValue(mockUser),
       } as any);
       const result = await service.findUserById(mockUser._id);
-      expect(userModel.findById).toHaveBeenCalledWith(mockUser._id);
-      expect(result).toEqual(mockUser);
+      expect(result).toEqual(mockUser as any);
     });
 
-      it('should throw NotFoundException if user is not found', async () => {
-        jest.spyOn(userModel, 'findById').mockReturnValue({
-          exec: jest.fn().mockResolvedValue(null),
-        } as any);
-        try {
-          await service.findUserById(mockUser._id);
-        } catch (error) {
-          expect(error.message).toEqual('Could not find user.');
-        }
-      });
+    it('should throw NotFoundException if user is not found', async () => {
+      jest.spyOn(userModel, 'findById').mockReturnValue({
+        exec: jest.fn().mockResolvedValue(null),
+      } as any);
+      try {
+        await service.findUserById(mockUser._id);
+      } catch (error) {
+        expect(error.message).toEqual('Could not find user.');
+      }
+    });
 
-      it('should throw NotFoundException if findById throws an error', async () => {
-        jest.spyOn(userModel, 'findById').mockReturnValue({
-          exec: jest.fn().mockRejectedValue(new Error('Some Error')),
-        } as any);
-        try {
-          await service.findUserById(mockUser._id);
-        } catch (error) {
-          expect(error.message).toEqual('Could not find user.');
-        }
-      });
+    it('should throw NotFoundException if findById throws an error', async () => {
+      jest.spyOn(userModel, 'findById').mockReturnValue({
+        exec: jest.fn().mockRejectedValue(new Error('Some Error')),
+      } as any);
+      try {
+        await service.findUserById(mockUser._id);
+      } catch (error) {
+        expect(error.message).toEqual('Could not find user.');
+      }
+    });
   });
 });
